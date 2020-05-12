7d7bd4a27085024061f6057c101fbc4777dd311a
nonari <alejandro.budino@gmail.com>
2020-05-13 00:15:05 +0200

    corrected recall

--- src/main/java/org/nonari/fic/ri/utils/evaluation/Evaluator.java
+++ src/main/java/org/nonari/fic/ri/utils/evaluation/Evaluator.java
@@ -1,6 +1,8 @@
 package org.nonari.fic.ri.utils.evaluation;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Evaluator {
@@ -22,27 +24,43 @@ public class Evaluator {
     }
 
     public void recallStandard() {
-        final List<Float> resultAt = new ArrayList<>();
+        final List<Float> precisionAt = new ArrayList<>();
+        final List<Float> recallAt = new ArrayList<>();
         int rFound = 0;
+        int count = 0;
         for (final int doc : this.result) {
             if (this.relev.contains(doc)) {
                 rFound++;
             }
-            resultAt.add(((float)rFound / (float) this.relev.size()));
+            count++;
+            precisionAt.add(rFound / (float)count);
+            recallAt.add(rFound / (float)this.relev.size());
         }
 
-        int point = 0;
-        for (final float r : resultAt) {
-             if ((point / 10) >= r) {
-
-             } else {
-                 while ((point / 10)<=r) {
-                     System.out.println("At " + (point / 10F) + ": " + r);
-
-                     point++;
-                 }
-             }
+        for (int point = 0; point <= 10; point++) {
+            final int fPos = first(recallAt, point / 10F);
+            final float p = max(precisionAt, fPos);
+            System.out.println("At " + point/10F + ":" + p);
         }
+    }
+
+    private int first(final List<Float> list, final float val) {
+        int pos = 0;
+        for (final float v : list) {
+            if (val <= v) {
+                break;
+            }
+            pos++;
+        }
+
+        return pos;
+    }
+
+    private float max(final List<Float> list, final int from) {
+        return list.stream()
+                .skip(from)
+                .max(Float::compareTo)
+                .get();
     }
 
     private int relevantDocsFound(final int at) {
